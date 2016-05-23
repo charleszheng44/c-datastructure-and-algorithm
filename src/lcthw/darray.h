80a58eca7eca4e67413271b55c466a3f4c33318d
Charles Zheng <charleszheng44@gmail.com>
2016-05-23 18:18:50 +0800

    fix typo

--- src/lcthw/darray.h
+++ src/lcthw/darray.h
@@ -16,7 +16,7 @@ void DArray_destroy(DArray *array);
 void DArray_clear(DArray *array);
 int DArray_expand(DArray *array);
 int DArray_contract(DArray *array);
-int DArray_push(DArray *array, void *ele);
+int DArray_push(DArray *array, void *el);
 void *DArray_pop(DArray *array);
 void DArray_clear_destory(DArray *array);
 
@@ -28,11 +28,11 @@ void DArray_clear_destory(DArray *array);
 
 #define DEFAULT_EXPAND_RATE 300
 
-static inline void DArray_set(DArray *array, int i, void *ele)
+static inline void DArray_set(DArray *array, int i, void *el)
 {
     check(i < array->max, "darray attempt to set past max.");
     if(i > array->end) array->end = i;
-    array->contents[i] = ele;
+    array->contents[i] = el;
 error:
     return;
 }
@@ -47,11 +47,11 @@ error:
 
 static inline void *DArray_remove(DArray *array, int i) 
 {
-    void *ele = array->contents[i];
+    void *el = array->contents[i];
 
     array->contents[i] = NULL;
 
-    return ele;
+    return el;
 }
 
 static inline void *DArray_new(DArray *array) 
